623c9dc46edec26619a28fdbe41b3479c025cbfb
강민제 <102217688+hsem4717@users.noreply.github.com>
2022-10-11 20:21:15 +0900

    Delete network directory

--- network/HRD_10.11/src/main/java/DBPKG/Util.java
+++
@@ -1,9 +0,0 @@
-package DBPKG;
-import java.sql.*;
-public class Util {
-	public static Connection getConnection() throws Exception{
-		Class.forName( "oracle.jdbc.OracleDriver");
-		Connection con = DriverManager.getConnection( "jdbc:oracle:thin:@//localhost:1521/XE", "system", "1234");
-		return con;
-	}
-}
